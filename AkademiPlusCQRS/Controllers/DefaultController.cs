68522cf3a1a3dfc8687a7949cd3bda456802e051
Ahmet Bogar <ahmetbogar@hotmail.com>
2023-06-13 02:43:02 +0300

    Add project files.

---
+++ AkademiPlusCQRS/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using AkademiPlusCQRS.CQRSPattern.Handlers;
+using AkademiPlusCQRS.CQRSPattern.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiPlusCQRS.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly GetProductQueryHandler _getProductQueryHandler;
+        private readonly GetProductByIDQueryHandler _getProductByIDQueryHandler;
+
+        public DefaultController(GetProductQueryHandler getProductQueryHandler, GetProductByIDQueryHandler getProductByIDQueryHandler)
+        {
+            _getProductQueryHandler = getProductQueryHandler;
+            _getProductByIDQueryHandler = getProductByIDQueryHandler;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _getProductQueryHandler.Handle();
+            return View(values);
+        }
+        public IActionResult GetProduct(int id)
+        {
+            var values=_getProductByIDQueryHandler.Handle(new GetProductByIDQuery(id));
+            return View(values);
+        }
+    }
+}
---
+++ AkademiPlusCQRS/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AkademiPlusCQRS.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-39VDBOA;initial catalog=AkademiPlusCQRSDb;integrated security=true");
+        }
+        public DbSet<Product>Products { get; set; }
+
+    }
+}
